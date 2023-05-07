ff44259fa463f1bdc882fe0b9c574b7eb0c4ee82
Joshua Zhuchenko <105894219+jzhuchenko2@users.noreply.github.com>
2023-05-07 17:42:56 -0500

    for loops

---
+++ forloop.ps1
@@ -0,0 +1,26 @@
+﻿#for-loop
+#$doctorstrange = @('josh','jake','bryan','jenny','jim')
+#For($counter =0;$counter -le ($doctorstrange.Length -1);$counter++){
+    #Write-Host "Holy smokes, it's" $doctorstrange[$counter]
+#}
+
+#Foreach ($peep in $doctorstrange) {
+    #Write-Host $peep "has arrived"
+#}
+
+##While-loop
+#$Xmen = @('Wolverine', 'Cyclops', 'Storm','jake')
+#$counter = 0
+#while($counter -ne 4) {
+    #Write-host $Xmen[$counter] "is a mutant!"
+    #$Xmen[$counter].Length
+    #$counter++;
+#}
+
+#Do
+$Xmen = @('Wolverine', 'Cyclops', 'Storm','jake')
+$counter = 0
+Do {
+    Write-Host $Xmen[$counter] "is a mutant!"
+    $counter++
+} while ($counter -ne 6)
