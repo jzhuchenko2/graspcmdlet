dfab0997a372f689f093133da153baf0a3958144
Joshua Zhuchenko <105894219+jzhuchenko2@users.noreply.github.com>
2023-05-06 22:33:25 -0500

    defining specific functions
    Launching problems to the user when some parameter is pinged

---
+++ definefunction.ps1
@@ -0,0 +1,12 @@
+﻿#defining the function
+function Test-SpaceX {
+    [cmdletBinding()] #turns into adv. function
+    param(
+        [Parameter(mandatory)]
+        [int32]$PingCount
+    )
+    Test-Connection spacex.com -Count $PingCount
+    Write-Error -Message "It's a trap!" -ErrorAction Stop
+}
+
+try { Test-SpaceX -ErrorAction Stop } catch { Write-Output "Launch porblemo!" Write-Output $_}
