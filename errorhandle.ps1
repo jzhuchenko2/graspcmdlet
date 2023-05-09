665bc59a7b9963314a49b623ab0679575e602f7f
Joshua Zhuchenko <105894219+jzhuchenko2@users.noreply.github.com>
2023-05-08 21:32:55 -0500

    more on pinging

---
+++ errorhandle.ps1
@@ -0,0 +1,6 @@
+﻿#this is a representation of pinging
+Get-Content -Path C:\Scripts\Example.txt | ForEach-Object { 
+    if (Test-Connection -ComputerName $_ -Quiet -Count 1){
+        Get-CimInstance -ComputerName $_ -ClassName 'joshsmagic'
+    }
+}
