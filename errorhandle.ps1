915be2bc596bc7c001d57262b00140f1bcaacdda
Joshua Zhuchenko <105894219+jzhuchenko2@users.noreply.github.com>
2023-05-08 21:37:16 -0500

    Update errorhandle.ps1
    better capturing the property of computerName

--- errorhandle.ps1
+++ errorhandle.ps1
@@ -1,6 +1,6 @@
 ﻿#this is a representation of pinging
 Get-Content -Path C:\Scripts\Example.txt | ForEach-Object { 
     if (Test-Connection -ComputerName $_ -Quiet -Count 1){
-        Get-CimInstance -ComputerName $_ -ClassName 'joshsmagic'
+        $operating_system = Get-CimInstance -ComputerName $_ -ClassName 'Win32_OperatingSystem' | Select-Object -Expand property Caption "$_ - $operating_system"
     }
 }
