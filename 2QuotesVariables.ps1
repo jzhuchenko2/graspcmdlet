ae4d3f24ab242d0d56246c1791664bf74dc50ef2
Joshua Zhuchenko <105894219+jzhuchenko2@users.noreply.github.com>
2023-05-09 20:25:34 -0500

    nerds lessons
    advancing tools

---
+++ 2QuotesVariables.ps1
@@ -0,0 +1,14 @@
+﻿
+[validateset("a","b","c")][String]$x = 'a'
+
+$i ="powershell"
+"this is the variable '$i, and $i rocks"
+
+$computername="client"
+Get-Service -name bits -ComputerName $computername | select MachineName, Name, Status
+
+$p = Get-Process lsass
+
+"Process id = $($p.id)"
+#$p is mainly the system.diagnostics.process
+
