8ba9a27ef01fdf6676f56f4e229792fa2182d13e
Joshua Zhuchenko <105894219+jzhuchenko2@users.noreply.github.com>
2023-05-12 20:38:59 -0500

    Parenthesis & Services
    assigning objects to certain services
    Use "gm" for getting methods

---
+++ paranthesis.ps1
@@ -0,0 +1,19 @@
+﻿#Object members and variables
+#Variables are very flexible
+$Service=Get-Service -Name bits
+$Service | gm 
+$Service.Status
+$Service.Start()
+$Msg="Service Name is $($service.name.ToUpper())"
+$msg
+#working with multiple objects
+$Services=Get-Service
+$services[0]
+$services[0].Status
+$services[-1].Name
+"Service Name is $($services[4].DisplayName)"
+"Service Name is $($services[4].name.ToUpper())"
+
+#paranthesis!!!!
+#create a txt & file
+'DC','Client' | Out-file c:\computers.txt
